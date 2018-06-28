060eb1285aac12836a16e6d9ff7e4569ab39816c
James Hunter <jameshunterbr@gmail.com>
2018-06-28 17:16:45 -0300

    revisão instalar pacotes

--- instalar_pacotes.R
+++ instalar_pacotes.R
@@ -1,14 +1,15 @@
 ## Programa para automatizar o download dos pacotes
 ## necessários para o curso Sustentare Análise de Dados com R
 ## Author: James Hunter
-## Date: 18/5/18
-## Version: 2.0
+## Date: 28/6/18
+## Version: 3.0
 
 pacotes <- c("tidyverse", "car", "caret", "caTools", "data.table", 
              "DescTools", "devtools", "gapminder", "ggpubr", "ggvis", 
-             "gmodels", "kableExtra", "knitr", "lattice", "lpSolve", 
-             "lubridate", "magrittr", "nortest", "nycflights13", "outliers", 
-             "pROC", "psych", "RColorBrewer", "Rcpp", "readxl", "ROCR",   
-             "shiny", "styler", "titanic", "yarrr")
+             "gmodels", "Hmisc", "kableExtra", "knitr", "lattice", 
+             "lpSolve", "lubridate", "magrittr", "mice", "nortest", 
+             "nycflights13", "outliers", "pROC", "psych", 
+             "RColorBrewer", "Rcpp", "readxl", "ROCR", "shiny", "styler", 
+             "titanic", "yarrr")
 
 install.packages(pacotes)
